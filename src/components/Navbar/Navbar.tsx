8b28990205889321474785ade3c783b45bf77eb0
Samuele Calcaterra <>
2023-05-28 16:42:51 +0200

    navbar component added

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = ({ pageTitle }: { pageTitle: string }) => {
+  return (
+    <div className="page-title-wrapper">
+      <h1 className="page-title">{pageTitle}</h1>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div>HOME WORKS</div>;
+  return (
+    <>
+      <Navbar pageTitle={"Michael Jackson"}></Navbar>
+    </>
+  );
 };
 
 export default Home;
