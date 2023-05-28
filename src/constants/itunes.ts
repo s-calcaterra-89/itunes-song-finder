247294bddb6ae92857dc6b237a15e16a37896fcc
Samuele Calcaterra <>
2023-05-28 20:06:09 +0200

    env variables endpoints

--- src/constants/itunes.ts
+++ src/constants/itunes.ts
@@ -1,2 +1,3 @@
-export const ITUNES_API_BASE_URL = "https://itunes.apple.com/";
-export const SONG_API_URL = "search";
+export const ITUNES_API_BASE_URL =
+  process.env.REACT_APP_ITUNES_API_BASE_URL || "https://itunes.apple.com/"; //Default value
+export const SONG_API_URL = process.env.REACT_APP_SONG_API_KEY || "search";
