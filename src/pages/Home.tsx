17b077f579346683e0b89e41ab650bc5722a2a23
Samuele Calcaterra <>
2023-05-28 17:28:51 +0200

    songlist list in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
+import SongList from "../components/SongList";
+import useGet from "../hooks/useGet";
+import { ITunesSongApiResponse } from "../models/itunes.model";
+import "./Home.css";
+
+const DEFAULT_ARTIST_NAME = "Michael Jackson";
 
 const Home = () => {
+  const [pageTitle, setPageTitle] = useState(DEFAULT_ARTIST_NAME);
+  const [searchName, setSearchName] = useState(DEFAULT_ARTIST_NAME);
+  const [retries, setRetries] = useState(0);
+
+  console.log("Home Render");
+  console.log("retries" + retries);
+  const { loading, isError, data } = useGet({ term: searchName, retries });
+
+  console.log("Loading : " + loading);
+  console.log("searchName : " + searchName);
+  console.log("isError : " + isError);
+  console.log("data : " + data);
+
+  const songs = (data as unknown as ITunesSongApiResponse)?.results;
   return (
     <>
       <Navbar pageTitle={"Michael Jackson"}></Navbar>
@@ -14,6 +35,11 @@ const Home = () => {
           return true;
         }}
       />
+      {songs && (
+        <>
+          <SongList songs={[...songs]} />
+        </>
+      )}
     </>
   );
 };
